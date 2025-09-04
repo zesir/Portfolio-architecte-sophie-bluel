e7f450f4fd7c88123225f0de5924b0f1e95bd389
slarz <bourdinflorent86@gmail.com>
2025-09-04 11:22:53 +0200

    feat: ajout de la fonction de tri et des classes css

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -5,6 +5,7 @@ const filtersContainer = document.querySelector(".filters");
 
 let allProjects = [];
 
+// connection avec l'api
 fetch("http://localhost:5678/api/works")
   .then((Response) => Response.json())
   .then((resultat) => {
@@ -14,6 +15,7 @@ fetch("http://localhost:5678/api/works")
     generateFilterButtons(allProjects); // générer les boutons de filtre
   });
 
+//affiche tous les projets
 function displayProjects(projects) {
   gallery.innerHTML = ""; // on vide la gallerie
   projects.forEach((project) => {
@@ -25,7 +27,7 @@ function displayProjects(projects) {
     gallery.appendChild(figure);
   });
 }
-
+//génère les boutons de filtres par catégories
 function generateFilterButtons(projects) {
   filtersContainer.innerHTML = "";
   const categories = [...new Set(projects.map((p) => p.category.name))]; // on recupere le nom des catégories dans un tableau
@@ -34,7 +36,21 @@ function generateFilterButtons(projects) {
     const button = document.createElement("button");
     button.classList.add("filter-btn");
     button.textContent = cat;
+    button.addEventListener("click", () => {
+      filterProjects(cat);
+      document
+        .querySelectorAll(".filter-btn")
+        .forEach((btn) => btn.classList.remove("selected"));
+      button.classList.add("selected");
+    });
 
     filtersContainer.appendChild(button);
   });
+
+  function filterProjects(categoryName) {
+    const filtered = allProjects.filter(
+      (project) => project.category.name === categoryName
+    );
+    displayProjects(filtered);
+  }
 }
